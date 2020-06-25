81b764b4f6e80370f0457ae5461fe20017257c76
hellow <myqy480@gmail.com>
2020-06-25 19:39:17 +0800

    add cors middleware

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false})); 
 app.use(bodyParser.json());
 
+// To prevent cors (Cors is a securtiy mechanism and force by browser )
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin','*'); //if we want specific url to access this api, we can change the * to that url
+  res.header('Access-Control-Allow-Header','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  if(req.method==="OPTIONS"){
+    res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
 //Routes which should handle request
 app.use('/products',productsRoutes);
 app.use('/orders',ordersRoutes);
